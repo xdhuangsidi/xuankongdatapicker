6a17d38fdff1e0a9b5e41f88e888a2762ffe406f
huangsidi <huangsidi@wps.cn>
2020-07-24 15:13:29 +0800

    添加天体计算三方库SwissEphemeris，用于计算太阳位置，用于确定节气从而确定月的干支

--- xuankongDataPicker/ViewController.m
+++ xuankongDataPicker/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <Masonry.h>
+#include "swephexp.h"
+#include "sweph.h"
+#include "math.h"
 
 @interface ViewController ()
 
@@ -16,6 +20,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UILabel *lable = [[UILabel alloc]init];
+    lable.textColor = [UIColor blackColor];
+    [self.view addSubview:lable];
+    [lable mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.centerY.mas_equalTo(self.view);
+    }];
+    double julday = swe_julday1(2020, 07, 22, 8, 37, YES);
+    double computerInfo[7];
+    char * errorInfo = (char *)malloc(600);
+    swe_calc_ut(julday, 0, 258, computerInfo, errorInfo);
+    double sunDegree = computerInfo[0];
+    lable.text = [NSString stringWithFormat:@"%f", sunDegree];
     // Do any additional setup after loading the view.
 }
 
